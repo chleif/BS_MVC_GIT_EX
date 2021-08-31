de03a2cd2dd7ce8e6142538a2605a7ad1221d4a8
chleif <chleif86741465@gmail.com>
2021-08-31 16:59:29 +0800

    加入專案檔案。

---
+++ BS_MVC_GIT_EX/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(BS_MVC_GIT_EX.Startup))]
+namespace BS_MVC_GIT_EX
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
